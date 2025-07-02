6033b46ddc71967969354a2ff321ed4ec75d6271
antonio.soares <163546672+antonioloofy@users.noreply.github.com>
2025-07-02 12:11:59 -0300

    Add files via upload

---
+++ TRABALHO_BDMY.sql
@@ -0,0 +1,8 @@
+SELECT * FROM mydb.professor;
+SELECT nome, email, area_atuacao
+FROM mydb.professor
+WHERE matricula = 'PROF2023002';
+
+SELECT nome, email, area_atuacao
+FROM mydb.professor
+WHERE matricula = 'PROF2023001';
